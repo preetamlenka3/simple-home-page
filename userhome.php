cb0a320229bdb501c27be718f7dc4ec44f026334
P charan lenka <preetamlenka3@gmail.com>
2022-02-20 19:41:37 +0530

    Add files via upload

---
+++ userhome.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>preetam lenka | web designer</title>
+	<link rel="stylesheet" type="text/css" href="homepage.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<h1>PORTFOLIO.</h1>
+			<ul id="nav">
+				<li><a class="homered" href="#">HOME</a></li>
+				<li><a class="homeblack" target="_blank" href="#">PORTFOLIO</a></li>
+				<li><a class="homeblack" target="_blank" href="#">ABOUT</a></li>
+				<li><a class="homeblack" target="_blank" href="#">QUALIFICATION</a></li>
+				<li><a class="homeblack" target="_blank" href="#">MY PROJECTS</a></li>
+				<li><a class="homeblack" target="_blank" href="#">CANTACT US!</a></li>
+				<li><a class="homeblack" style="color: #041EF8;" target="_blank" href="#">SIGN UP!</a></li>
+			</ul>
+		</nav>
+	</header>
+	<div class="divider"></div>
+
+	<div class="slider">
+		<img class="users" src="image/pic.jpg">
+		<h2 class="homesub">PREETAM LENKA <span style="color: white;">A Web DEVELOPER</span></h2>
+		<h1 class="homesho"></h1>
+		<h1 class="homelon">Hi! my name is preetam lenka. i am a web developer,<br>
+		                    and I'm very passionate and dedicated to my work.<br>
+		                     i have acquired the skills and knowledge necessary<br>
+		                      to make your project a success. i enjoy every step of<br> 
+		                  the design process, from discussion and collaboration.</h1>
+		<br>
+		<button class="buttonhome"><a style="text-decoration: none;" target="_blank" href="#">HIRE ME!</a></button>
+		<div class="copyright-text">
+			&copy; 2022 made with <span>&#x2764;</span> by preetam
+	    </div>
+	</div>
+
+</body>
+</html>
